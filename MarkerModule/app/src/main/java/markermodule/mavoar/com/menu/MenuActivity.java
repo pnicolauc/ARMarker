2d0f8488d90487b0c7e2ecff3551e8f79c026c2d
Machinezero <80.nicolau@gmail.com>
2017-02-21 23:30:29 +0000

    adding config file support

--- MarkerModule/app/src/main/java/markermodule/mavoar/com/menu/MenuActivity.java
+++ MarkerModule/app/src/main/java/markermodule/mavoar/com/menu/MenuActivity.java
@@ -4,12 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
-import android.widget.TextView;
-
-import java.util.ArrayList;
 
 import markermodule.mavoar.com.R;
 import markermodule.mavoar.com.markers.ImageTargets;
